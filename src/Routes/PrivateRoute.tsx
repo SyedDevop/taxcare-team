215f04c4fd079d2a483a803651c8fb24994e168a
Syed Devops <syeds.devops007@gmail.com>
2023-12-24 22:28:38 +0530

    fix: type error fro PrivateRoute

--- src/Routes/PrivateRoute.tsx
+++ src/Routes/PrivateRoute.tsx
@@ -3,12 +3,15 @@ import { Route, Redirect, RouteProps } from "react-router-dom";
 
 import { useAuth } from "../Hooks";
 
-const PrivateRoute: React.FC<RouteProps> = ({ children, ...rest }) => {
+interface PrivateRouteProps extends RouteProps {
+  children: React.ReactNode;
+}
+
+const PrivateRoute: React.FC<PrivateRouteProps> = ({ children, ...rest }) => {
   const { user } = useAuth();
   return (
     <Route
       {...rest}
-      //@ts-ignore
       render={({ location }) =>
         user === null ? (
           children
