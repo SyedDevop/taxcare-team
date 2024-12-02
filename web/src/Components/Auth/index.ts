aeec6385cf80e7afd6d2e5e66f841d42e1fb632e
SyedDevop <syeds.devops007@gmail.com>
2024-12-03 00:36:55 +0530

    remove (Auth)

--- web/src/Components/Auth/index.ts
+++
@@ -1,2 +0,0 @@
-export { LoginForm } from "./LoginForm";
-export { ForgetPassword } from "./ForgetPassword/ForgetPassword";
--- web/src/Components/Auth/useLoginForm.ts
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-export const useLoginFormState = () => {
-  const [loginFormState, setLoginFormState] = useState(false);
-  function updateLoginForm() {
-    setLoginFormState((preState) => !preState);
-  }
-
-  return { loginFormState, updateLoginForm, setLoginFormState };
-};
